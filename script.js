1a1d5af7bffe9c06cb0cb09af459d5c14df1dc25
aasya110 <132384311+aasya110@users.noreply.github.com>
2024-11-30 03:15:00 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('create-event-form')?.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Event created successfully!');
+});
+
+document.getElementById('register-form')?.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Registered successfully!');
+});
